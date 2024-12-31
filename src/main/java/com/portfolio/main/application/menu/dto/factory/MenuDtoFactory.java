bb923bcebf5211f1ec08e72b314b50508c29dc74
iii5412 <iii5412@gmail.com>
2024-12-31 10:25:41 +0900

    빈 순환참조 수정

--- src/main/java/com/portfolio/main/application/menu/dto/factory/MenuDtoFactory.java
+++
@@ -1,7 +0,0 @@
-package com.portfolio.main.application.menu.dto.factory;
-
-import com.portfolio.main.application.menu.dto.MenuDto;
-
-public interface MenuDtoFactory<T extends MenuDto> {
-    T create(T source);
-}
--- src/main/java/com/portfolio/main/domain/service/menu/menurole/MenuRoleService.java
+++ src/main/java/com/portfolio/main/domain/service/menu/menurole/MenuRoleService.java
@@ -1,6 +1,8 @@
 package com.portfolio.main.domain.service.menu.menurole;
 
+import com.portfolio.main.application.menu.dto.MenuDto;
 import com.portfolio.main.application.menurole.dto.SaveMenuRole;
+import com.portfolio.main.application.role.dto.RoleDto;
 import com.portfolio.main.domain.service.account.role.RoleService;
 import com.portfolio.main.domain.model.account.Role;
 import com.portfolio.main.domain.model.menu.Menu;
@@ -11,20 +13,33 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 @Transactional
 @AllArgsConstructor
 public class MenuRoleService {
+    private final MenuService menuService;
+    private final RoleService roleService;
     private final MenuRoleRepository menuRoleRepository;
 
     public List<MenuRole> findByMenuId(Long menuId) {
         return menuRoleRepository.findByMenuId(menuId);
     }
 
-    public MenuRole save(Menu menu, Role role) {
+    public MenuRole save(Long menuId, Long roleId) {
+        final Menu menu = menuService.findById(menuId);
+        final Role role = roleService.findById(roleId);
+        final List<Role> flattenedAboveLevel = roleService.findFlattenedAboveLevel(role.getLevel());
         final MenuRole menuRole = new MenuRole(new MenuRole.MenuRoleId(menu.getId(), role.getId()), menu, role);
+
+        List<MenuRole> savedMenuRoles = new ArrayList<>();
+
+        flattenedAboveLevel
+                .forEach(role -> {
+                    
+                });
 
         return menuRoleRepository.save(menuRole);
     }

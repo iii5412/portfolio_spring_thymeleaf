34bc71fd2cb27df8b434111e7823fd6cebe8def9
iii5412 <iii5412@gmail.com>
2025-02-18 17:14:32 +0900

    tuiGrid 로컬방식으로 변경

--- src/main/resources/static/js/tuigrid/tui.grid-4.21.22/packages/toast-ui.grid/src/store/exports.ts
+++
@@ -1,6 +0,0 @@
-import { Exports } from '@t/store/export';
-import { observable } from '../helper/observable';
-
-export function create({ excelCompatibilityMode = false }: Exports) {
-  return observable<Exports>({ excelCompatibilityMode });
-}
--- src/main/resources/static/js/tuigrid/tui.grid-4.21.22/packages/toast-ui.vue-grid/vue.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  configureWebpack: {
-    resolve: {
-      // https://cli.vuejs.org/guide/troubleshooting.html#symbolic-links-in-node-modules
-      symlinks: false,
-    },
-  },
-};
